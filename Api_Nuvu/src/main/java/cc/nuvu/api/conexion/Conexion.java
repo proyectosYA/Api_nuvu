cf295523c6333a301f573644530629c28e57cdda
yio <proyectosweb067@gmail.com>
2020-11-30 23:14:43 -0500

    cambio de war

--- Api_Nuvu/src/main/java/cc/nuvu/api/conexion/Conexion.java
+++ Api_Nuvu/src/main/java/cc/nuvu/api/conexion/Conexion.java
@@ -17,15 +17,15 @@ import java.util.logging.Logger;
  */
 public class Conexion {
 
-    private final String URL = "jdbc:mysql://localhost:3306/nuvu_db";
+    private final String DATABASE_URL = "jdbc:postgresql://ec2-54-237-135-248.compute-1.amazonaws.com:5432/d8i5ajfgkvuobv";
 
     public static Conexion conexion;
     private Connection con;
 
     private Conexion(){
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection(URL, "root", "root");
+            Class.forName("org.postgresql.Driver");
+            con = DriverManager.getConnection(DATABASE_URL, "jqktfptruqdjgl", "12f199914c104febd1b722f94c1ea2ec93ce67bbeebbbc2418920ac31bfebb15");
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SQLException ex) {
